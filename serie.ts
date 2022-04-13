205323845fe60fa00d821cdc6caa21bd321a70b5
mar-cas3 <61592394+mar-cas3@users.noreply.github.com>
2022-04-13 16:19:02 -0500

    Entrega - 202210_Taller_TypeScript_1

--- serie.ts
+++ serie.ts
@@ -1,3 +1,3 @@
 export class Serie {
-    constructor(public id:number, public name:string, public channel:string, public seasons:number, public description:string, public website:string, public image:string){}
+    constructor(public id: number, public name: string, public channel: string, public seasons: number, public description: string, public website: string, public image: string) { }
 }
